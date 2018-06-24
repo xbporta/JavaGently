fd72165fee02ad5816944c09a520e20571c30d5b
xbporta <xbporta@gmail.com>
2018-06-24 18:29:21 +0200

    Added Example 02.01 - Welcome

---
+++ A_02_01_Welcome/Welcome.java
@@ -0,0 +1,15 @@
+class Welcome {
+
+    /* Welcome program by J M Bishop Dec 1996
+     * ----------------Java 2 April 2000
+     * Illustrates a simple program displaying a message
+     */
+
+    Welcome() {
+        System.out.println("Welcome to Java Gently!");
+    }
+
+    public static void main(String[] args) {
+        new Welcome();
+    }
+}
