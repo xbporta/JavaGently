5bd2a08b4ffeef49d43c89535f8c7cfcdd9ee39e
xbporta <xbporta@gmail.com>
2018-07-20 00:15:59 +0200

    Fixed some typos

--- C_03_Types and Methods/A_03_01_Random investigation/RandomInvestigation.java
+++ C_03_Types and Methods/A_03_01_Random investigation/RandomInvestigation.java
@@ -1,13 +1,14 @@
 class RandomInvestigation {
+
 	RandomInvestigation() {
 		double random1, random2;
 		random1 = Math.random();
 		random2 = Math.random();
 		System.out.println("The numbers are: " + random1 + " and " + random2);
-		System.out..println("Max is " + Math.max(random1, random2));
+		System.out.println("Max is " + Math.max(random1, random2));
 	}
 	
-	public static void (String [] args) {
+	public static void main (String [] args) {
 		new RandomInvestigation();
 	}
 }
